9546d23cea30ce957e5eaf1e6761a06bbf14c6e5
B@blu1234 <bablubambal1999@gmail.com>
2022-01-29 09:02:18 +0400

    first cart

---
+++ src/data.js
@@ -0,0 +1,8 @@
+const data = {
+    products:[
+        {id:1,name:"Mackbook",price:1400,image:"https://picsum.photos/id/180/2400/1600"}, //180/2400/1600
+        {id:2,name:"Old Car",price:1400,image:"https://picsum.photos/id/111/4400/2656"}, // id/111/4400/2656
+        {id:3,name:"W-Shoes",price:1400,image:"https://picsum.photos/id/21/3008/2008"}, //21/3008/2008
+    ]
+}
+export default data;
